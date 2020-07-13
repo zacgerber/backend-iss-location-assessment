ae8689b339bcd3a4047ec58b6e0951ba2820ad2c
zac Gerber <Zgerber91@gmail.com>
2020-07-13 14:12:20 -0400

    iss gif moves to actual iss posion and there is a dot where Indianapolis is on map

--- iss.py
+++ iss.py
@@ -6,6 +6,7 @@ import time
 
 url = 'http://api.open-notify.org/astros.json'
 response = urllib.request.urlopen(url)
+
 result = json.loads(response.read())
 
 print('People in Space: ', result['number'])
@@ -21,36 +22,40 @@ response = urllib.request.urlopen(url)
 result = json.loads(response.read())
 
 location = result['iss_position']
-lat = float(location['latitude'])
-lon = float(location['longitude'])
-timestamp = result['timestamp']
+lat = location['latitude']
+lon = location['longitude']
 
 print('Latitude: ', lat)
 print('Longitude: ', lon)
 
+new_lat = float(lat)
+new_lon = float(lon)
 
-earth = "map.gif"
 screen = turtle.Screen()
+
 screen.setup(720, 360)
 screen.setworldcoordinates(-180, -90, 180, 90)
-screen.bgpic(earth)
+screen.register_shape(
+    '/Users/zacgerber/zacs-quarter3/backend-iss-location-assessment/iss.gif')
+screen.bgpic(
+    '/Users/zacgerber/zacs-quarter3/backend-iss-location-assessment/map.gif')
 
-screen.register_shape('iss.gif')
+# screen.register_shape('iss.gif')
 iss = turtle.Turtle()
-iss.shape('iss.gif')
+iss.shape(
+    '/Users/zacgerber/zacs-quarter3/backend-iss-location-assessment/iss.gif')
 iss.setheading(90)
 iss.penup()
-screen.exitonclick()
-iss.goto(lon, lat)
+iss.goto(new_lon, new_lat)
 
 
 # Indianapolis
-# lat = 39.768452
-# lon = -86.156212
+lat = float(39.768452)
+lon = float(-86.156212)
 
 # Space Center, Houston
-# lat = 29.5502
-# lon = -95.097
+# lat = float(29.5502)
+# lon = float(-95.097)
 
 location = turtle.Turtle()
 location.penup()
@@ -58,18 +63,13 @@ location.color('yellow')
 location.goto(lon, lat)
 location.dot(5)
 location.hideturtle()
+screen.exitonclick()
 
-url = 'https://spotthestation.nasa.gov/tracking_map.cfm' + str(float(lat))
-+ '&lon=' + str(float(lon))
+url = 'http://api.open-notify.org/iss-pass.json?lat=' + str(lat)
++ '&lon=' + str(lon)
 response = urllib.request.urlopen(url)
 result = json.loads(response.read())
-print(result)
+# print(result)
 
 over = result['response'][1]['risetime']
-style = ('Ariel', 6, 'bold')
-location.write(time.ctime(over), font=style)
-# print(over)
-
-
-if screen is not None:
-    screen.exitonclick()
+location.write(time.ctime(over))
