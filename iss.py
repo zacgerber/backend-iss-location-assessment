39b063df4e6a8982465142d2a7f255078c288341
zac Gerber <Zgerber91@gmail.com>
2020-07-15 14:04:31 -0400

    getting timstamp on Indy location

--- iss.py
+++ iss.py
@@ -52,7 +52,9 @@ iss.goto(new_lon, new_lat)
 # Space Center, Houston
 # lat = float(29.5502)
 # lon = float(-95.097)
-
+url = f'http://api.open-notify.org/iss-pass.json?lat={lat}&lon={lon}'
+response = urllib.request.urlopen(url)
+result = json.loads(response.read())
 # Indianapolis
 lat = 39.768452
 lon = -86.156212
@@ -62,19 +64,24 @@ location.color('yellow')
 location.goto(lon, lat)
 location.dot(5)
 location.hideturtle()
+over = result['response'][1]['risetime']
+time.ctime(over)
+style = ('Arial', 12, 'normal')
+location.write(over, align='center', font=style)
 
 if screen is not None:
     print('Click on screen to exit ...')
     screen.exitonclick()
 
-
-# url = 'http://api.open-notify.org/iss-pass.json?lat={}&lon={}'.format(
-#     lat, lon)
+# url = f'http://api.open-notify.org/iss-pass.json?lat={lat}&lon={lon}'
 # response = urllib.request.urlopen(url)
 # result = json.loads(response.read())
-# # response = requests.get(url)
-# # result = json.loads(response.rea())
-
-# over = response.json()['response'][0]['risetime']
+# response = requests.get(url).read()
+# response = urllib.request.urlopen(url)
+# result = json.loads(response.rea())
+# location.goto(lon, lat)
+# location.dot(5)
+# location.hideturtle()
+# over = result['response'][1]['risetime']
 # style = ('Arial', 12, 'normal')
-# location.write(time.ctime(over), font=style)
+# location.write(time.ctime(over, font=style))
